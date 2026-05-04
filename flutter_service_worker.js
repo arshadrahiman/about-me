624119480fc0e7652f281eebdfe19bae7c9d1b48
arshadrahiman <arshadrahiman007@gmail.com>
2026-05-04 15:18:15 +0530

    Update projects with real names and original logos

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "79bbf4f7c2f51bf93479097ce2f2e00b",
+const RESOURCES = {"flutter_bootstrap.js": "79baa45993895a5a8134c34f13ea6f6d",
 "version.json": "b0a2c87b57f6646cb3a59698fb9fc619",
 "index.html": "a4459e7bc6539f99874d2f7b7a4f418f",
 "/": "a4459e7bc6539f99874d2f7b7a4f418f",
-"main.dart.js": "96aa62096b40ff44bbb307a81f3243d0",
+"main.dart.js": "d9914bb6503000a75bfc89f0940fc153",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
