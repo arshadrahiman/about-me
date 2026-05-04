0e3051cd78d96d02c56972c473dd486fb187fefd
arshadrahiman <arshadrahiman007@gmail.com>
2026-05-05 00:11:24 +0530

    Deploy: Localization and language toggle features (Tue May  5 00:11:24 IST 2026)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "7bbb9d4c85a057a6c93d91ad3fccde3a",
+const RESOURCES = {"flutter_bootstrap.js": "42bf1439f9cf46b1b8d6e23889933d9c",
 "version.json": "b0a2c87b57f6646cb3a59698fb9fc619",
 "index.html": "fcbd59d80b5d9e5d05205cacbe1dbf10",
 "/": "fcbd59d80b5d9e5d05205cacbe1dbf10",
-"main.dart.js": "2f6f16635d4160dbc2cc04a836646b59",
+"main.dart.js": "5ed858a948a58a130ceac8b36a0032de",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -25,7 +25,7 @@ const RESOURCES = {"flutter_bootstrap.js": "7bbb9d4c85a057a6c93d91ad3fccde3a",
 "assets/packages/font_awesome_flutter/lib/fonts/Font-Awesome-7-Free-Solid-900.otf": "5b8d20acec3e57711717f61417c1be44",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "8737ba405ad5dc39102293d8303f0a4a",
-"assets/fonts/MaterialIcons-Regular.otf": "46a847f118766ac4f5c1df4533fb348e",
+"assets/fonts/MaterialIcons-Regular.otf": "baf2dff37a327f56e9b8eaa116e88fbd",
 "assets/assets/images/google_developer.jpeg": "9a200ffdb25b4edb8656baca076b81b3",
 "assets/assets/icons/xcode.png": "651833f53ba0e1c76587d4aee018ca5e",
 "assets/assets/icons/roadradar.png": "37cfd75ce95dd76e37de257bc365ea5e",
