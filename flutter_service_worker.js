022e2cb469d615f3cab68db8e2b6453629f95eac
arshadrahiman <arshadrahiman007@gmail.com>
2026-05-04 15:33:24 +0530

    Add Web Development section to Services

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "61057d56c9478ac926c927fcd3655f10",
+const RESOURCES = {"flutter_bootstrap.js": "0c4616b1e2919ddaa711853d74c0d85d",
 "version.json": "b0a2c87b57f6646cb3a59698fb9fc619",
 "index.html": "a4459e7bc6539f99874d2f7b7a4f418f",
 "/": "a4459e7bc6539f99874d2f7b7a4f418f",
-"main.dart.js": "d19f648b8bace5604e0bce0b889f0d1a",
+"main.dart.js": "b1c3c1383d62d5e56c6cb9aceb36c912",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
